37d94886bede775142487e231635423ae8fc30d5
bryansteele <bryan@bryansteele.me>
2019-07-04 23:28:00 -0400

    mario less - style fix

--- week1/mario.c
+++ week1/mario.c
@@ -1,11 +1,13 @@
+// Ask user to pick height and then print outhash steps according to user's answer
 #include <cs50.h>
 #include <stdio.h>
 
 int get_positive_int (string prompt);
 
 int main(void)
+// Prompt user for height.
 {
-    int height = get_positive_int("Please choose a height between 1 and 8 (inclusive) and enter it now: ");
+    int height = get_positive_int("=> Please choose a height between 1 and 8 (inclusive) and enter it now: ");
     
     for (int i = 1; i <= height; i++)
     {
@@ -21,6 +23,7 @@ int main(void)
         printf ("\n");
     }
 }
+// Prompt user for positive integer
 int get_positive_int (string prompt)
 {
     int n;
@@ -29,8 +32,5 @@ int get_positive_int (string prompt)
         n = get_int ("%s", prompt);
     }
     while (n < 1 || n > 8);
-    return n;
-    
+    return n;   
 }
-    
-
