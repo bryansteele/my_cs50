42bd78748a9bf0d90b0991322d9482048e294f23
bryansteele <bryan@bryansteele.me>
2019-07-26 22:31:16 -0400

    fixes a few style issues

--- week1/mario.c
+++ week1/mario.c
@@ -7,12 +7,12 @@ int get_positive_int (string prompt);
 int main(void)
 // Prompt user for height.
 {
-    int height = get_positive_int("=> Please choose a height between 1 and 8 (inclusive) and enter it now: ");
+    int height = get_positive_int ("=> Please choose a height between 1 and 8 (inclusive) and enter it now: ");
     
     for (int i = 1; i <= height; i++)
     {
         int a;
-        for (a = 0; a <= height-i-1; a++)
+        for (a = 0; a <= height - i - 1; a++)
         {
             printf(" ");
         }
