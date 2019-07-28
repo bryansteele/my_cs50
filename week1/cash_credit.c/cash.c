b220430079f0a547814468c760533299dc61ba3e
bryansteele <bryan@bryansteele.me>
2019-07-28 07:20:08 -0400

    Fixes styling

--- week1/cash_credit.c/cash.c
+++ week1/cash_credit.c/cash.c
@@ -1,5 +1,6 @@
-// Asks the user how much change is owed and then prints the minimum number of coins
-// with which that change can be made (.25, .10, .05, and .01).
+// Asks the user how much change is owed and then prints the
+// minimum  number of coins using: quarters, dimes, nickles and
+// pennies.
 
 #include <cs50.h>
 #include <math.h>
@@ -8,40 +9,41 @@
 float get_positive_float(string prompt);
 
 int main(void)
-    // Prompt user for how much change is owed
+// Prompts user for the amount of changed owed
 {
-    float change_to_be_returned = get_positive_float("=> Please enter the amount of change owed: ");                               
+    float change_to_be_returned = get_positive_float("=> Please enter the amount of change owed: ");
+
     int change = round(change_to_be_returned * 100);
-    
+
     int coins = 0;
     while (change >= 25)
     {
         coins++;
         change -= 25;
     }
-    
+
     while (change >= 10)
     {
         coins++;
         change -= 10;
     }
-    
+
     while (change >= 5)
     {
         coins++;
         change -= 5;
     }
-    
+
     while (change >= 1)
     {
         coins++;
         change -= 1;
     }
-    
-    printf("The minimal number of coins that can be returned is: %i\n",  coins); 
+
+    printf("The minimal number of coins that can be returned is: %i\n",  coins);
 }
 
-// Prompt user for positive integer
+// Prompts user for positive integer
 float get_positive_float(string prompt)
 {
     float x;
@@ -50,5 +52,5 @@ float get_positive_float(string prompt)
         x = get_float("%s", prompt);
     }
     while (x < 0);
-    return x;   
+    return x;
 }
