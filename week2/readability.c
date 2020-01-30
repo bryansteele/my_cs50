d93a3cf879955ac61af9c8a4aecbf9ac601d7662
bryansteele <bryan@bryansteele.me>
2020-01-30 16:32:03 -0500

    Adds the counting of words.

--- week2/readability.c
+++ week2/readability.c
@@ -1,18 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string text = get_string("Please enter a string of text.\n==> ");
 
-
-    int count = 0;
-    for (int i = 0; text[i] != '\0'; i++)
+    int letters = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
         if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <='Z'))
         {
-            count++;
+            letters++;
         }
     }
-    printf("==> %i\n", count);
+    printf("==> %i\n", letters);
+
+    int words = 0;
+    for (int j = 0, x = strlen(text); j < x; j++)
+    {
+        if (text[j] == ' ' && text[j + 1] != ' ')
+        {
+            words++;
+        }
+    }
+    printf("==> %i\n", words + 1);
 }
