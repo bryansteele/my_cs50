947151448caa5a6f5aef070638bfddb62afca17f
bryansteele <bryan@bryansteele.me>
2020-01-30 17:51:01 -0500

    extracts into 3 different functions

--- week2/readability.c
+++ week2/readability.c
@@ -2,27 +2,58 @@
 #include <stdio.h>
 #include <string.h>
 
+int counts_letters(string str);
+int counts_words(string str);
+int counts_sentences(string str);
+
 int main(void)
 {
     string text = get_string("Please enter a string of text.\n==> ");
 
-    int letters = 0;
-    for (int i = 0, n = strlen(text); i < n; i++)
+    int num_letters = counts_letters(text);
+    int num_words = counts_words(text);
+    int num_sentences = counts_sentences(text);
+
+    printf("==> %i letter(s)\n", num_letters);
+    printf("==> %i word(s)\n", num_words + 1);
+    printf("==> %i sentence(s)\n", num_sentences);
+}
+
+int letters = 0;
+int counts_letters(string str)
+{
+    for (int i = 0, n = strlen(str); i < n; i++)
     {
-        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <='Z'))
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <='Z'))
         {
             letters++;
         }
     }
-    printf("==> %i\n", letters);
+    return letters;
+}
 
-    int words = 0;
-    for (int j = 0, x = strlen(text); j < x; j++)
+int words = 0;
+int counts_words(string str)
+{
+    for (int i = 0, n = strlen(str); i < n; i++)
     {
-        if (text[j] == ' ' && text[j + 1] != ' ')
+        if (str[i] == ' ' && str[i + 1] != ' ')
         {
             words++;
         }
     }
-    printf("==> %i\n", words + 1);
+    return words;
+}
+
+int sentences = 0;
+int counts_sentences(string str)
+{
+    for (int i = 0, n = strlen(str); i < n; i++)
+    {
+        if (str[i] == '.' || str[i] == '!' || str[i] == '?')
+        {
+            sentences++;
+        }
+    }
+    return sentences;
 }
